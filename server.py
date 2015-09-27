f5510beed597f669b16fc5a8d58bee764950d832
Jason R. Coombs <jaraco@jaraco.com>
2015-09-27 16:22:05 -0400

    Server should bind to ::0 to support IPv4 and IPv6.

--- server.py
+++ server.py
@@ -20,5 +20,5 @@ def factory():
     return SmtpProtocol(Handler())
 
 loop = asyncio.get_event_loop()
-server = loop.run_until_complete(loop.create_server(factory, '0.0.0.0', 9978))
+server = loop.run_until_complete(loop.create_server(factory, '::0', 9978))
 loop.run_forever()
