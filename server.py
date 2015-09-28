532b1dc5b76e550b2d7cc38423e480cf67998757
Andrew Kuchling <amk@amk.ca>
2015-09-27 20:17:16 -0400

    Set the REUSEADDR option on the socket

--- server.py
+++ server.py
@@ -23,6 +23,7 @@ def factory():
 loop = asyncio.get_event_loop()
 sock = socket.socket(socket.AF_INET6, socket.SOCK_STREAM)
 sock.setsockopt(socket.IPPROTO_IPV6, socket.IPV6_V6ONLY, False)
+sock.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, True)
 sock.bind(('::0', 9978))
 server = loop.run_until_complete(loop.create_server(factory, sock=sock))
 loop.run_forever()
