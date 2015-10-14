0ff2637b2ffb59bb498890b5ef193705602782e0
Barry Warsaw <barry@python.org>
2015-10-13 22:16:37 -0400

    LMTP.

---
+++ aiosmtpd/lmtp.py
@@ -0,0 +1,25 @@
+__all__ = [
+    'LMTP',
+    ]
+
+
+import asyncio
+
+from aiosmtpd.smtp import SMTP
+
+
+class LMTP(SMTP):
+    @asyncio.coroutine
+    def smtp_LHLO(self, arg):
+        """The LMTP greeting, used instead of HELO/EHLO."""
+        yield from super().smtp_HELO(arg)
+
+    @asyncio.coroutine
+    def smtp_HELO(self, arg):
+        """HELO is not a valid LMTP command."""
+        yield from self.push('500 Error: command "HELO" not recognized')
+
+    @asyncio.coroutine
+    def smtp_EHLO(self, arg):
+        """EHLO is not a valid LMTP command."""
+        yield from self.push('500 Error: command "EHLO" not recognized')
