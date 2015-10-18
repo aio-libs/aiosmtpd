a7e7c40aa953e122461147b8a61fa5014164ffa3
Barry Warsaw <barry@python.org>
2015-10-18 14:53:03 -0400

    Test another code path.

--- aiosmtpd/tests/test_server.py
+++ aiosmtpd/tests/test_server.py
@@ -8,13 +8,29 @@ __all__ = [
 
 import unittest
 
+from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Sink
-from aiosmtpd.smtp import SMTP
+from aiosmtpd.smtp import SMTP as Server
+from smtplib import SMTP
+
+
+class UTF8Controller(Controller):
+    def factory(self):
+        return Server(self.handler, enable_SMTPUTF8=True)
 
 
 class TestServer(unittest.TestCase):
     def test_constructor_contraints(self):
         # These two arguments cannot both be set.
-        self.assertRaises(ValueError, SMTP, Sink(),
+        self.assertRaises(ValueError, Server, Sink(),
                           enable_SMTPUTF8=True,
                           decode_data=True)
+
+    def test_smtp_utf8(self):
+        controller = UTF8Controller(Sink())
+        controller.start()
+        self.addCleanup(controller.stop)
+        with SMTP(controller.hostname, controller.port) as client:
+            code, response = client.ehlo('example.com')
+        self.assertEqual(code, 250)
+        self.assertIn(b'SMTPUTF8', response.splitlines())
