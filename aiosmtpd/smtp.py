594dccbaed232a8f91424867f7c93ebe3ab749cc
kozzztik <kozzztik@mail.ru>
2017-03-25 09:42:41 -0400

    Flake fix

--- aiosmtpd/smtp.py
+++ aiosmtpd/smtp.py
@@ -7,7 +7,6 @@ from .base_handler import BaseHandler, SMTPException
 from email._header_value_parser import get_addr_spec, get_angle_addr
 from email.errors import HeaderParseError
 from public import public
-from warnings import warn
 
 
 try:
