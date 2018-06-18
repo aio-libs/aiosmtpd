262ab1664343a3874a692257efea4821b56d7e03
Justin Clift <justin@postgresql.org>
2018-06-18 22:25:04 +0100

    Fix LHLO service incorrectly waiting for smtp_HELO
    Copied directly from Timothe Litt's fix in #123

--- aiosmtpd/lmtp.py
+++ aiosmtpd/lmtp.py
@@ -7,7 +7,7 @@ class LMTP(SMTP):
     @syntax('LHLO hostname')
     async def smtp_LHLO(self, arg):
         """The LMTP greeting, used instead of HELO/EHLO."""
-        await super().smtp_HELO(arg)
+        await super().smtp_EHLO(arg)
         self.show_smtp_greeting = False
 
     async def smtp_HELO(self, arg):
