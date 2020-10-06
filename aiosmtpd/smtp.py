2114f71086b045a15ef03da7be404f522e579180
Pandu POLUAN <pepoluan@gmail.com>
2020-10-06 21:16:15 +0700

    Fix for Semmle/LGTM.com alert

--- aiosmtpd/smtp.py
+++ aiosmtpd/smtp.py
@@ -3,8 +3,8 @@ import socket
 import asyncio
 import logging
 import collections
+import asyncio.sslproto as sslproto
 
-from asyncio import sslproto
 from email._header_value_parser import get_addr_spec, get_angle_addr
 from email.errors import HeaderParseError
 from public import public
