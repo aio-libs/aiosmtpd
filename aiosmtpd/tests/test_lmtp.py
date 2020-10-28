cf16e74084e02fef1a4a836cef369528f956738e
Pandu POLUAN <pepoluan@gmail.com>
2020-10-28 16:36:01 +0700

    Convert test_lmtp.py to pytest

--- aiosmtpd/tests/test_lmtp.py
+++ aiosmtpd/tests/test_lmtp.py
@@ -1,12 +1,12 @@
 """Test the LMTP protocol."""
 
+import pytest
 import socket
-import unittest
 
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Sink
 from aiosmtpd.lmtp import LMTP
-from aiosmtpd.testing.helpers import SMTP_with_asserts
+from smtplib import SMTP
 
 
 class LMTPController(Controller):
@@ -14,43 +14,46 @@ class LMTPController(Controller):
         return LMTP(self.handler)
 
 
-class TestLMTP(unittest.TestCase):
-    def setUp(self):
-        controller = LMTPController(Sink)
-        controller.start()
-        self.addCleanup(controller.stop)
+@pytest.fixture(autouse=True)
+def lmtp_controller():
+    controller = LMTPController(Sink)
+    controller.start()
+    #
+    yield controller
+    #
+    controller.stop()
 
-        self.client = SMTP_with_asserts(self, from_=controller)
-        self.addCleanup(self.client.quit)
 
-    def test_lhlo(self):
-        self.client.assert_cmd_resp(
-            "LHLO example.com",
-            (250, bytes(socket.getfqdn(), 'utf-8'))
-        )
+@pytest.fixture
+def client(lmtp_controller: LMTPController):
+    client = SMTP(lmtp_controller.hostname, lmtp_controller.port)
+    #
+    yield client
+    #
+    client.quit()
 
-    def test_helo(self):
-        # HELO and EHLO are not valid LMTP commands.
-        resp = self.client.helo('example.com')
-        self.assertEqual(
-            (500, b'Error: command "HELO" not recognized'),
-            resp,
-        )
 
-    def test_ehlo(self):
-        # HELO and EHLO are not valid LMTP commands.
-        resp = self.client.ehlo('example.com')
-        self.assertEqual(
-            (500, b'Error: command "EHLO" not recognized'),
-            resp,
-        )
+def test_lhlo(client):
+    resp = client.docmd("LHLO example.com")
+    assert resp == (250, bytes(socket.getfqdn(), 'utf-8'))
 
-    def test_help(self):
-        # https://github.com/aio-libs/aiosmtpd/issues/113
-        self.client.assert_cmd_resp(
-            "HELP",
-            (250,
-             b'Supported commands: AUTH DATA HELP LHLO MAIL '
-             b'NOOP QUIT RCPT RSET VRFY'
-             )
-        )
+
+def test_helo(client):
+    # HELO and EHLO are not valid LMTP commands.
+    resp = client.helo('example.com')
+    assert resp == (500, b'Error: command "HELO" not recognized')
+
+
+def test_ehlo(client):
+    # HELO and EHLO are not valid LMTP commands.
+    resp = client.ehlo('example.com')
+    assert resp == (500, b'Error: command "EHLO" not recognized')
+
+
+def test_help(client):
+    # https://github.com/aio-libs/aiosmtpd/issues/113
+    resp = client.docmd("HELP")
+    assert resp == (250,
+                    b'Supported commands: AUTH DATA HELP LHLO MAIL '
+                    b'NOOP QUIT RCPT RSET VRFY'
+                    )
