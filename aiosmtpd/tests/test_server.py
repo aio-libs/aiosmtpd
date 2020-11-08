49f947a9b8581d6df16ecf2262f782e4f5ab34cf
Pandu POLUAN <pepoluan@gmail.com>
2020-11-08 12:38:42 +0700

    Convert test_server.py to pytest

--- aiosmtpd/tests/test_server.py
+++ aiosmtpd/tests/test_server.py
@@ -1,13 +1,12 @@
 """Test other aspects of the server implementation."""
 
 import os
+import pytest
 import socket
-import unittest
 
 from aiosmtpd.controller import Controller
 from aiosmtpd.handlers import Sink
 from aiosmtpd.smtp import SMTP as Server
-from smtplib import SMTP
 
 
 def in_wsl():
@@ -17,42 +16,36 @@ def in_wsl():
     return os.environ.get("PLATFORM") == "wsl"
 
 
-class TestServer(unittest.TestCase):
-    def test_smtp_utf8(self):
-        controller = Controller(Sink())
-        controller.start()
-        self.addCleanup(controller.stop)
-        with SMTP(controller.hostname, controller.port) as client:
-            code, response = client.ehlo('example.com')
-        self.assertEqual(code, 250)
-        self.assertIn(b'SMTPUTF8', response.splitlines())
+class TestServerNieuw:
+    def test_smtp_utf8(self, base_controller, client):
+        code, mesg = client.ehlo("example.com")
+        assert code == 250
+        assert b"SMTPUTF8" in mesg.splitlines()
 
     def test_default_max_command_size_limit(self):
         server = Server(Sink())
-        self.assertEqual(server.max_command_size_limit, 512)
+        assert server.max_command_size_limit == 512
 
     def test_special_max_command_size_limit(self):
         server = Server(Sink())
-        server.command_size_limits['DATA'] = 1024
-        self.assertEqual(server.max_command_size_limit, 1024)
+        server.command_size_limits["DATA"] = 1024
+        assert server.max_command_size_limit == 1024
 
-    @unittest.skipIf(in_wsl(), "WSL prevents socket collisions")
-    # See explanation in the in_wsl() function
-    def test_socket_error(self):
-        # Testing starting a server with a port already in use
-        s1 = Controller(Sink(), port=8025)
-        s2 = Controller(Sink(), port=8025)
-        self.addCleanup(s1.stop)
-        self.addCleanup(s2.stop)
-        s1.start()
-        self.assertRaises(socket.error, s2.start)
+    @pytest.mark.skipif(in_wsl(), reason="WSL prevents socket collision")
+    def test_socket_error(self, base_controller):
+        contr2 = Controller(Sink(), port=8025)
+        try:
+            with pytest.raises(socket.error):
+                contr2.start()
+        finally:
+            contr2.stop()
 
     def test_server_attribute(self):
         controller = Controller(Sink())
-        self.assertIsNone(controller.server)
+        assert controller.server is None
         try:
             controller.start()
-            self.assertIsNotNone(controller.server)
+            assert controller.server is not None
         finally:
             controller.stop()
-            self.assertIsNone(controller.server)
+        assert controller.server is None
