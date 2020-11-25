d8b49049ca36fb047adf7af53bb95f5d9251e80d
Pandu POLUAN <pepoluan@gmail.com>
2020-11-25 09:42:13 +0700

    Add docstring to test_1packaging.py

--- aiosmtpd/tests/test_1packaging.py
+++ aiosmtpd/tests/test_1packaging.py
@@ -6,5 +6,6 @@ from packaging import version
 
 
 def test_versioning():
+    """Ensure version number compliance to PEP-440"""
     ver = version.parse(a_smtp.__version__)
-    assert isinstance(ver, version.Version), "Version number must comply with PEP-0440"
+    assert isinstance(ver, version.Version), "Version number must comply with PEP-440"
