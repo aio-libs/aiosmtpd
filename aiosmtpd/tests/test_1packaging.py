67d206b8e0796a38812bb20c086b171b7041cdb0
Pandu POLUAN <pepoluan@gmail.com>
2020-12-01 11:25:43 +0700

    Add test for version desc in NEWS.rst

--- aiosmtpd/tests/test_1packaging.py
+++ aiosmtpd/tests/test_1packaging.py
@@ -4,8 +4,10 @@ import pytest
 import subprocess
 import aiosmtpd.smtp as a_smtp
 
+from itertools import tee
 # noinspection PyPackageRequirements
 from packaging import version
+from pathlib import Path
 
 
 RE_DUNDERVER = re.compile(r"\s*__version__\s?=\s?(['\"])(?P<ver>[^'\"]+)\1\s*$")
@@ -36,3 +38,24 @@ class TestVersion:
             pytest.fail(f"Cannot find __version__ in {reference}!")
         master_ver = version.parse(m.group("ver"))
         assert aiosmtpd_version >= master_ver, "Version number cannot be < master's"
+
+
+class TestDocs:
+    def test_NEWS_version(self, aiosmtpd_version):
+        news_rst = next(Path("..").rglob("*/NEWS.rst"))
+        with open(news_rst, "rt") as fin:
+            # pairwise() from https://docs.python.org/3/library/itertools.html
+            a, b = tee(fin)
+            next(b, None)
+            for ln1, ln2 in zip(a, b):
+                if not ln1[0].isdigit():
+                    continue
+                ln1 = ln1.strip()
+                ln2 = ln2.strip()
+                if ln2 != ("=" * len(ln1)):
+                    continue
+                break
+        newsvers = ln1.split()[0]
+        newsver = version.parse(newsvers)
+        if newsver.base_version < aiosmtpd_version.base_version:
+            pytest.fail("NEWS.rst is not updated")
