d432ff579ac7ef45e5caa88a16ce57b69d415706
Pandu POLUAN <pepoluan@gmail.com>
2021-02-11 14:31:11 +0700

    Use .parent.parent instead of "../.."

--- aiosmtpd/docs/conf.py
+++ aiosmtpd/docs/conf.py
@@ -34,7 +34,7 @@ RE__VERSION = re.compile(r"""__version__ = (['"])(?P<ver>[^'"]+)(\1)""")
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-repo_root = Path("../..").expanduser().absolute()
+repo_root = Path(".").expanduser().absolute().parent.parent
 
 
 def syspath_insert(pth: Path):
