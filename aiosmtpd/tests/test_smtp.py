e46a82e2358edf8877cfd5fc26ab505a5ef723c2
Pandu POLUAN <pepoluan@gmail.com>
2021-03-02 11:23:24 +0700

    Add some tests
    * Test longest possible email for MAIL FROM and RCPT TO if
      local_part_limit=0
    * Verify if limit set to N, address with local part up to N works

--- aiosmtpd/tests/test_smtp.py
+++ aiosmtpd/tests/test_smtp.py
@@ -490,15 +490,20 @@ class TestSMTP(_CommonMethods):
         "$A12345@example.com",
         "!def!xyz%abc@example.com",
         "a" * 65 + "@example.com",  # local-part > 64 chars -- see Issue#257
+        "b" * 488 + "@example.com",  # practical longest for MAIL FROM
+        "c" * 500,  # practical longest domainless for MAIL FROM
     ]
 
     valid_rcptto_addresses = valid_mailfrom_addresses + [
         # Postmaster -- RFC5321 § 4.1.1.3
         "<Postmaster>",
+        "b" * 490 + "@example.com",  # practical longest for RCPT TO
+        "c" * 502,  # practical longest domainless for RCPT TO
     ]
 
     invalid_email_addresses = [
-        "<@example.com>",  # no local part
+        "<@example.com>",  # null local part
+        "<johnathon@>",  # null domain part
     ]
 
     @pytest.mark.parametrize("data", [b"\x80FAIL\r\n", b"\x80 FAIL\r\n"])
@@ -1662,8 +1667,11 @@ class TestCustomization(_CommonMethods):
     def test_limitlocalpart(self, plain_controller, client):
         plain_controller.smtpd.local_part_limit = 64
         client.ehlo("example.com")
-        locpart = "a" * 65
+        locpart = "a" * 64
         resp = client.docmd(f"MAIL FROM: {locpart}@example.com")
+        assert resp == S.S250_OK
+        locpart = "b" * 65
+        resp = client.docmd(f"RCPT TO: {locpart}@example.com")
         assert resp == S.S553_MALFORMED
 
 
