b6d3c26a3e58d915f93b87091f7b970e463ba72b
Sam Bull <git@sambull.org>
2024-02-05 01:28:05 +0000

    Update aiosmtpd/tests/test_server.py

--- aiosmtpd/tests/test_server.py
+++ aiosmtpd/tests/test_server.py
@@ -314,7 +314,7 @@ class TestController:
         assert get_localhost() == "127.0.0.1"
         assert mock_hasip6.called
 
-    async def test_getlocalhost_6yes(self, mocker: MockFixture):
+    def test_getlocalhost_6yes(self, mocker: MockFixture):
         mock_sock = mocker.Mock()
         mock_makesock: mocker.Mock = mocker.patch("aiosmtpd.controller.makesock")
         mock_makesock.return_value.__enter__.return_value = mock_sock
