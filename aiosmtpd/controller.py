3ed6df07b61d0b120d34a7d06748d8c8df430bda
Pēteris Caune <cuu508@gmail.com>
2026-05-09 13:37:48 +0300

    Update UnixSocketMixin to disable socket cleanup on Py >= 3.13
    Starting from Python 3.13, the unix listener automatically cleans up
    its socket from the filesystem on shutdown. This can be disabled by
    passing in cleanup_socket=False. I think it makes sense for aiosmtpd
    to do that, to keep the shutdown behavior consistent between Python versions.
    
    This change fixes the only failing test on Python 3.13:
    
    aiosmtpd/tests/test_server.py::TestUnthreaded::test_unixsocket

--- aiosmtpd/controller.py
+++ aiosmtpd/controller.py
@@ -5,6 +5,7 @@ import asyncio
 import errno
 import os
 import ssl
+import sys
 import threading
 import time
 from abc import ABCMeta, abstractmethod
@@ -447,10 +448,18 @@ class UnixSocketMixin(BaseController, metaclass=ABCMeta):  # pragma: no-unixsock
         Does NOT actually start the protocol object itself;
         _factory_invoker() is only called upon fist connection attempt.
         """
+        kw = {}
+        if sys.version_info >= (3, 13, 0):
+            # Starting from python 3.13, the unix listener automatically
+            # cleans up its socket from the filesystem on shutdown. This can
+            # be disabled by passing in cleanup_socket=False. We do that here to keep
+            # the shutdown behavior consistent between Python versions.
+            kw["cleanup_socket"] = False
         return self.loop.create_unix_server(
             self._factory_invoker,
             path=self.unix_socket,
             ssl=self.ssl_context,
+            **kw
         )
 
     def _trigger_server(self):
